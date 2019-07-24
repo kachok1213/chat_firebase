8c8599c9db0b6c6104c4bfcd7c7256b0c71a450e
unknown <amiperisr@gmail.com>
2019-07-24 07:34:53 -0700

    chat_firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import firebase from "firebase"
+import {Input,List, ListItem, ListItemText,Button,TextField} from "@material-ui/core"
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+        text:"",
+        messages:[],
+        uname:"amit"
+    }
+}   
+componentWillMount(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyD0HUbmW02lzVaQN3TMzEInzcgHCGT_jHM",
+    authDomain: "app-management-2b3b5.firebaseapp.com",
+    databaseURL: "https://app-management-2b3b5.firebaseio.com",
+    projectId: "app-management-2b3b5",
+    storageBucket: "app-management-2b3b5.appspot.com",
+    messagingSenderId: "51691846536",
+    appId: "1:51691846536:web:cc1339ad60135cca"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  this.getMessages()
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+onSubmit=(event)=>{
+  if(this.state.uname=="amit"){
+    if(event.charCode=== 13 && this.state.text.trim() !== ""){
+      console.log(this.state.uname+":\n"+this.state.text)
+      this.writeMessageToDb(this.state.uname+":\n"+this.state.text)
+      this.setState({text:""})
+      this.setState({uname:"timor"})
+    }
+  }
+  else{
+    if(event.charCode=== 13 && this.state.text.trim() !== ""){
+      console.log(this.state.uname+":\n"+this.state.text)
+      this.writeMessageToDb(this.state.uname+":\n"+this.state.text)
+      this.setState({text:""})
+      this.setState({uname:"amit"})
+    }
+  }
+ 
+}
+
+writeMessageToDb = (message) =>{
+firebase
+.database()
+.ref("Chat/")
+.push({
+  text:message})
+}
+
+getMessages = () =>{
+  var messagesDb = firebase.database().ref("Chat/")
+  messagesDb.on("value",snapshot =>{
+    let newMessages = [] 
+    snapshot.forEach(child =>{
+      var message = child.val()
+      newMessages.push({id:child.key,text:message.text})
+    })
+    this.setState({messages:newMessages})
+  })
+}
+
+renderMessages = () =>{
+  return this.state.messages.map((message)=> 
+    <ListItem>
+     <ListItemText>{message.text} </ListItemText>
+    </ListItem>)
+}
+
+render() {
+
+    return (
+        <div style={{width:300,margin:"auto"}}>
+          
+<List>
+{this.renderMessages()}
+</List>
+
+<TextField 
+autoFocus={true}
+multiline={true}
+fullWidth={true}
+rowsMax={3}
+placeholder="Type somthing"
+onChange={event => this.setState({text:event.target.value})}
+value={this.state.text}
+onKeyPress={this.onSubmit}
+
+/>
+
+
+        </div>
+    );
+ }
 }
 
 export default App;
+  
